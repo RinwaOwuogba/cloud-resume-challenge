5401e71e353365502fdde3922a827cc7116df4fe
bhorlarh <bhorlarinwah@gmail.com>
2022-02-27 11:39:22 +0100

    :wrench: updated lb config

--- infra/load_balancer.tf
+++ infra/load_balancer.tf
@@ -22,6 +22,34 @@ module "gce-lb-https" {
   ssl               = true
   private_key       = tls_private_key.example.private_key_pem
   certificate       = tls_self_signed_cert.example.cert_pem
+
+  backends = {
+    default = {
+      description                     = null
+      protocol                        = "HTTP"
+      port                            = 80
+      port_name                       = "http"
+      timeout_sec                     = 10
+      connection_draining_timeout_sec = null
+      enable_cdn                      = false
+      security_policy                 = null
+      session_affinity                = null
+      affinity_cookie_ttl_sec         = null
+      custom_request_headers          = null
+      custom_response_headers         = null
+
+      health_check = local.health_check
+      log_config = {
+        enable      = true
+        sample_rate = 1.0
+      }
+      iap_config = {
+        enable               = false
+        oauth2_client_id     = ""
+        oauth2_client_secret = ""
+      }
+    }
+  }
 }
 
 
