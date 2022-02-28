7863da88952c55d57fe764ebe6d2dc53c6f05a0c
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-02-28 15:17:04 +0000

    :gear: fixed terraform formatting.

--- infra/db.tf
+++ infra/db.tf
@@ -1,6 +1,6 @@
 # Create default app engine application to activate firestore database
 resource "google_app_engine_application" "app" {
-  project     = google_project.my_project.project_id
-  location_id = var.region
+  project       = google_project.my_project.project_id
+  location_id   = var.region
   database_type = "CLOUD_FIRESTORE"
 }
