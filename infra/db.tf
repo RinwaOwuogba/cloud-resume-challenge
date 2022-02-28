946cc807526711fdc967083fddf72558e84487bf
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2022-02-28 17:15:16 +0000

    :gear: added app engine admin api activation to terraform config.

--- infra/db.tf
+++ infra/db.tf
@@ -3,4 +3,7 @@ resource "google_app_engine_application" "app" {
   project       = var.project
   location_id   = var.region
   database_type = "CLOUD_FIRESTORE"
+  depends_on = [
+    google_project_service.app_engine
+  ]
 }
